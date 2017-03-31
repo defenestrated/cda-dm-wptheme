82989bd444dcb34e567b2e1d1e39f85d36c8bade
Sam Galison <samface@gmail.com>
2017-03-31 16:06:05 -0400

    removed unnecessary img attr from header

--- functions.php
+++ functions.php
@@ -436,7 +436,6 @@ function spitup_logo () {
     $food = '<a href="' .  esc_url( home_url( '/' ) ) . '" rel="home">';
     $food .= '<img src="' . get_header_image() . '"'
           . ' srcset="' .  esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ) . '"'
-          . ' sizes="' .  esc_attr( $custom_header_sizes ). '"'
           . ' width="' .  esc_attr( get_custom_header()->width ) . '"'
           . ' height="' .  esc_attr( get_custom_header()->height ) . '"'
           . ' alt="' .  esc_attr( get_bloginfo( 'name', 'display' ) ) . '">';
