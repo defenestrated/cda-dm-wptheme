5ce909e85ccaf7d403728362d5379237c086c7d9
Sam Galison <samface@gmail.com>
2017-06-07 11:26:26 -0400

    style fixes

--- header.php
+++ header.php
@@ -56,12 +56,12 @@
                                     <?php echo spitup_logo('text') ?>
                                 </div>
 						                    <!-- <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p> -->
-					                  <?php endif;
-
-					                  $description = get_bloginfo( 'description', 'display' );
-					                  if ( $description || is_customize_preview() ) : ?>
-						                    <p class="site-description"><?php echo $description; ?></p>
 					                  <?php endif; ?>
+
+					                  <?php //$description = get_bloginfo( 'description', 'display' );
+					                  //if ( $description || is_customize_preview() ) : ?>
+						                <!-- <p class="site-description"><?php //echo $description; ?></p> -->
+					                  <?php /*endif;*/ ?>
 				                </div><!-- .site-branding -->
 
 				                <?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
